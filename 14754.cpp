e65378c08d8395b4a0a99d424e1e5593fb9021dd
ATEZ <shieldnet@users.noreply.github.com>
2018-01-16 17:33:18 +0900

    Create 14754.cpp

---
+++ 14754.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+typedef long long LL;
+
+LL map[1001][1001] = { 0, };
+bool check[1001][1001] = { false, };
+
+int main() {
+	LL n, m;
+	LL answer = 0;
+	scanf("%lld %lld", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%lld", &map[i][j]);
+		}
+	}
+
+	//Row
+	for (int i = 0; i < n; i++) {
+		int maxval = -1;
+		int maxpos = -1;
+		for (int j = 0; j < m; j++) {
+			if (map[i][j] >= maxval) {
+				maxpos = j;
+				maxval = map[i][j];
+			}
+		}
+		for (int j = 0; j < m; j++) {
+			if (j == maxpos) check[i][j] = true;
+		}
+	}
+
+	//Col
+	for (int i = 0; i < m; i++) {
+		int maxval = -1;
+		int maxpos = -1;
+		for (int j = 0; j < n; j++) {
+			if (map[j][i] >= maxval) {
+				maxpos = j;
+				maxval = map[j][i];
+			}
+		}
+		for (int j = 0; j < n; j++) {
+			if (j == maxpos) check[j][i] = true;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			answer += check[i][j] ? 0 : map[i][j];
+		}
+	}
+
+	printf("%lld", answer);
+	return 0;
+}
